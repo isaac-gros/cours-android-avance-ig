835f192f2aa67d8686932a999378205f013a3098
Isaac Gros <isaac.gros888@gmail.com>
2019-10-31 10:39:45 +0100

    Finalisation du tutoriel Google

--- app/src/main/java/com/example/insanelywrongmechanics/MainActivity.kt
+++ app/src/main/java/com/example/insanelywrongmechanics/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.example.insanelywrongmechanics
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toastMe(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun increment(view: View) {
+        val textView = findViewById<TextView>(R.id.currentNumber)
+        val countString = textView.text.toString()
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        textView.text = count.toString()
+    }
+
+    fun genRandom(view: View) {
+        // Create an Intent to start the second activity
+        val randomIntent = Intent(this, SecondActivity::class.java)
+
+        // Get the ID of the current number
+        val textView = findViewById<TextView>(R.id.currentNumber)
+
+        // Convert text content of textView in a string...
+        val countString = textView.text.toString()
+
+        // ...then as a number
+        val count: Int = Integer.parseInt(countString)
+
+        // Add the count to the extras for the Intent.
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
+
+        // Start the new activity.
+        startActivity(randomIntent)
     }
 }
